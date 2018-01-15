5ac945ee0cc468c009bc7d9fa7f6be9c523d0d77
acc044 <michel.kapell@haw-hamburg.de>
2018-01-15 16:13:26 +0100

    constraint gebaut

--- src/net/AllDifferentConstraint.java
+++ src/net/AllDifferentConstraint.java
@@ -1,5 +1,13 @@
 package net;
 
+import core.ConstType;
+
 public class AllDifferentConstraint extends Constraint {
+
+
+
+    public Boolean check(int firstValue, int secondValue) {
+    return false;
+    }
 
 }
---
+++ src/net/BinaerConstraint.java
@@ -0,0 +1,66 @@
+package net;
+
+import core.ConstType;
+
+public class BinaerConstraint extends Constraint {
+    private Knot linkerWert;
+    private Knot rechterWert;
+
+    private ConstType type;
+
+    public BinaerConstraint(Knot linkerWert, Knot rechterWert, ConstType type) {
+        this.linkerWert = linkerWert;
+        this.rechterWert = rechterWert;
+        this.type = type;
+    }
+
+    public Boolean check(int firstValue, int secondValue) {
+        Boolean result = false;
+
+        switch (type) {
+            case GLEICH: result = gleich(firstValue,secondValue);
+                break;
+            case UNAER: result = unaer(firstValue,secondValue);
+                break;
+            case NACHBAR: result = nachbar(firstValue,secondValue);
+                break;
+            case LINKERNACHBAR: result = linkerNachbar(firstValue,secondValue);
+                break;
+            case RECHTERNACHBAR: result = rechterNachbar(firstValue,secondValue);
+                break;
+        }
+
+        return result;
+    }
+
+    private Boolean rechterNachbar(int firstValue, int secondValue) {
+        Boolean result = false;
+        result = (firstValue-secondValue)==1;
+        return result;
+    }
+
+    private Boolean linkerNachbar(int firstValue, int secondValue) {
+        Boolean result = false;
+        result = (firstValue-secondValue)==-1;
+        return result;
+    }
+
+    private Boolean nachbar(int firstValue, int secondValue) {
+        Boolean result = false;
+        result = linkerNachbar(firstValue,secondValue)||rechterNachbar(firstValue,secondValue);
+        return result;
+    }
+
+    private Boolean unaer(int firstValue, int secondValue) {
+        Boolean result = false;
+        return result;
+    }
+
+    private Boolean gleich(int firstValue, int secondValue) {
+        Boolean result = false;
+        result= firstValue==secondValue;
+        return result;
+    }
+
+
+}
