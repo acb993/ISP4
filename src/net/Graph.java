0ab0ae890fe0169ec35214193ac1f1b345e58036
Anastasios Palatiou <anastasios.palatiou@haw-hamburg.de>
2018-01-15 13:49:54 +0100

    Init

--- src/net/Graph.java
+++ src/net/Graph.java
@@ -1,4 +1,8 @@
 package net;
 
+import java.util.List;
+
 public class Graph {
+    private List<Constraint> kanten;
+    private List<Knot> knoten;
 }
--- src/net/Knot.java
+++ src/net/Knot.java
@@ -1,4 +1,9 @@
 package net;
 
+import java.util.List;
+
 public class Knot {
+    private List<Integer> wertebereich;
+    private Integer konkreterwert;
+
 }
